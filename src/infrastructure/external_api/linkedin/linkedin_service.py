c89e7a97ae434046ce0b759ad45541abcc9ad5e3
Phillipe R Souza <8622005+phillrog@users.noreply.github.com>
2026-02-05 04:03:45 -0300

    ajuste scope

--- src/infrastructure/external_api/linkedin/linkedin_service.py
+++ src/infrastructure/external_api/linkedin/linkedin_service.py
@@ -18,7 +18,7 @@ class LinkedInService(IAuthService):
             "response_type": "code",
             "client_id": self.client_id,
             "redirect_uri": self.redirect_uri,
-            "scope": "openid profile email",
+            "scope": "openid profile",
             "state": state_combinado
         }
         request_url = requests.Request("GET", self.auth_url, params=params).prepare().url
